16396bec813483c4476041ef8ed64ec45b5ac302
vidassundk <vidassundk@gmail.com>
2025-05-28 09:07:40 +0200

    Initial Commit

---
+++ TjuTjuTests/TjuTjuTests.swift
@@ -0,0 +1,17 @@
+//
+//  TjuTjuTests.swift
+//  TjuTjuTests
+//
+//  Created by Vidas Sun on 28/05/2025.
+//
+
+import Testing
+@testable import TjuTju
+
+struct TjuTjuTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
