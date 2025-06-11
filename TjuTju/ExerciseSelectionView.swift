d590d6aeedb4954b813d28378701dbe3945e3100
vidassundk <vidassundk@gmail.com>
2025-06-11 11:32:52 +0200

    Progress on Workout Creation

---
+++ TjuTju/ExerciseSelectionView.swift
@@ -0,0 +1,18 @@
+//
+//  ExerciseSelectionView.swift
+//  TjuTju
+//
+//  Created by Vidas Sun on 09/06/2025.
+//
+
+import SwiftUI
+
+struct ExerciseSelectionView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ExerciseSelectionView()
+}
---
+++ TjuTju/Models/UserPreferences.swift
@@ -0,0 +1,8 @@
+//
+//  UserPreferences.swift
+//  TjuTju
+//
+//  Created by Vidas Sun on 10/06/2025.
+//
+
+import Foundation
